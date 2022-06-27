b262945ec7ffd72525fd31fac29d264f14bb67b0
xoan3012 <108244100+xoan3012@users.noreply.github.com>
2022-06-27 20:10:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ const CONFIG = {
     btnYes: 'Đồng ý',
     btnNo: 'Không nha :(((((((((',
     question: 'Nếu cái icon nó chạy hoài làm My không chọn được đáp án thì thử gõ đáp án heyyyyy',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'My đồng ý :v :v :v..........................................................................',
+    btnReply: 'Gửi cho Tuấn',
+    reply: 'My đồng ý :v :v :v :v :v :v :v :v :v :v :v :v :v :v :v :v :v :v :v :v :v',
     mess: 'Có thể câu trả lời là có hoặc không, thì T cũng muốn có 1 cơ hội 😘',
-    messDesc: 'Hẹn My trưa Thứ 4 cho Tuấn mời cơm 1 bữa nha',
-    btnAccept: 'Click để biết ai xinh nhất quả đất',
+    messDesc: 'Hẹn My trưa Thứ 4 cho Tuấn mời cơm 1 bữa nha (hẹn lui 1 ngày để My có thời gian sắp xếp)',
+    btnAccept: 'Click Here <3',
     messLink: 'https://i.imgur.com/m469WXQ.jpg'
 }
