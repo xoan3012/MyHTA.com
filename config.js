5ac376f8eb6c59f53456ec57601d8b105c912cb6
xoan3012 <108244100+xoan3012@users.noreply.github.com>
2022-06-27 08:52:34 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "For MyHTA",
+    introTitle: 'Hà Thị Ái My',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^Tu bi con ti niu^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😘',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Tất nhiên là My cũng thích Tuấn rồi.......Nhấn Enter đi nào..................................',
+    mess: 'Mình biết mà <3. 😘😘',
+    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    btnAccept: 'Click để biết ai xinh nhất quả đất',
+    messLink: 'https://i.imgur.com/Twk8Fij.jpg'
+}
