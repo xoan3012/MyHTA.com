5f3f7ac10030f142bb9142dd27a0ea144cc50f65
xoan3012 <108244100+xoan3012@users.noreply.github.com>
2022-07-01 17:18:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ const CONFIG = {
     introTitle: 'Hà Thị Ái My',
     introDesc: `Có những lương duyên khi bắt đầu (dù rất nhanh) đã chắc chắn như “ván đã đóng thuyền”<br>
     có những duyên phận khi bắt đầu (dù dài hơi) đã định trước là sẽ phải ra đi<br>
-    Có những duyên phận chẳng bao giờ có kết quả tốt đẹp.`
+    Có những duyên phận chẳng bao giờ có kết quả tốt đẹp.`,
     btnIntro: '^^Tu bi con ti niu^^',
     title: 'Nếu đã là Duyên, thì dù có xa cách thế nào cũng tìm được đường gặp lại. <br>Nếu đã là Nợ, thì dù có trốn tránh tới đâu cũng không thể thoát được. <br> Và khi đã là Phận, thì đơn giản, là không thể chống lại…',
-    desc: `Chuyện tình cảm, cứ để nó tự nhiên, nếu có duyên sẽ chẳng sợ lạc đường`
+    desc: `Chuyện tình cảm, cứ để nó tự nhiên, nếu có duyên sẽ chẳng sợ lạc đường`,
     btnYes: 'Đồng ý',
     btnNo: 'Không nha :(((((((((',
     question: 'Giờ biết cho My gõ cái gì nữa đây :v :v :v',
