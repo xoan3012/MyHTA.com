f7140cdc96b724a80366d41450a8160f368ac53c
xoan3012 <108244100+xoan3012@users.noreply.github.com>
2022-07-20 09:24:19 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var message = "Trang web đã được thay đổi nội dung trước đó vào ngày 01/07/2022, Click OK nếu My muốn xem tiếp nội dung";
+alert(message);
 $(document).ready(function() {
     // process bar
     setTimeout(function() {
