f11abbf1b08ea4c3dd6aa95675ca67cf268c76eb
xoan3012 <108244100+xoan3012@users.noreply.github.com>
2022-08-11 21:00:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    titleWeb: "For MyHTA",
+    titleWeb: "Anniversary 6-8/2022",
     introTitle: 'Hà Thị Ái My',
     introDesc: `Có những lương duyên khi bắt đầu (dù rất nhanh) đã chắc chắn như “ván đã đóng thuyền”
     có những duyên phận khi bắt đầu (dù dài hơi) đã định trước là sẽ phải ra đi.
     Có những duyên phận chẳng bao giờ có kết quả tốt đẹp.`,
-    btnIntro: '^^Tu bi con ti niu^^',
-    title: 'Nếu đã là Duyên, thì dù có xa cách thế nào cũng tìm được đường gặp lại. Nếu đã là Nợ, thì dù có trốn tránh tới đâu cũng không thể thoát được. Và khi đã là Phận, thì đơn giản, là không thể chống lại…',
-    desc: `Chuyện tình cảm, cứ để nó tự nhiên, nếu có duyên sẽ chẳng sợ lạc đường`,
-    btnYes: 'Đồng ý',
-    btnNo: 'Không nha :(((((((((',
+    btnIntro: '^^Tu bi không tình iu^^',
+    title: 'Chuyện tình cảm, cứ để nó tự nhiên, nếu có duyên sẽ chẳng sợ lạc đường…',
+    desc: `I liked you before I knew your name`,
+    btnYes: 'Không',
+    btnNo: 'Đồng ý :(((((((((',
     question: 'Giờ biết cho My gõ cái gì nữa đây :v :v :v',
-    btnReply: 'Gửi cho Tuấn',
-    reply: 'Hôm nào rảnh My mời nước nha, chúng ta sẽ nói chuyện như 2 người bạn :v :v :v :v :v :v :v :v :v :v :v :v :v :v :v :v :v :v :v :v :v',
-    mess: 'Không biết My còn vào lại trang này để xem không :v :v :v',
-    messDesc: 'Chúc My luôn thành công trên con đường mình đã chọn',
+    btnReply: 'Gửi thông điệp đến vũ trụ',
+    reply: 'Ngày em đến, em dạy anh cách yêu thương một người. Ngày em đi, em chưa dạy anh cách quên đi một người anh ...thương.......................<3 <3 <3 <3',
+    mess: 'I have good memories',
+    messDesc: 'Chúc Em luôn thành công trên con đường của mình',
     btnAccept: 'Click Here <3',
     messLink: 'https://i.imgur.com/m469WXQ.jpg'
 }
